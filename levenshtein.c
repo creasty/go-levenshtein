bbe48a93ccefd7ba78be9b086979415db10e8a5f
Yuki Iwanaga <yuki@creasty.com>
2016-09-28 23:37:18 +0900

    Implement in c

---
+++ levenshtein.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+int
+levenshtein(char *a, int a_size, char *b, int b_size)
+{
+    if (a == b) {
+        return 0;
+    }
+    if (a_size == 0) {
+        return b_size;
+    }
+    if (b_size == 0) {
+        return a_size;
+    }
+
+    unsigned int *m = calloc(a_size, sizeof(unsigned int));
+
+    unsigned int ia = 0;
+    unsigned int ib = 0;
+    unsigned int da;
+    unsigned int db;
+    unsigned int d;
+
+    while (ia < a_size) {
+        m[ia] = ia + 1;
+        ia++;
+    }
+
+    while (ib < b_size) {
+        char c = b[ib];
+        d = da = ib++;
+        ia = -1;
+
+        while (++ia < a_size) {
+            db = (c == a[ia]) ? da : da + 1;
+            da = m[ia];
+
+            m[ia] = d = (da > d)
+                ? (db > d ? d + 1 : db)
+                : (db > da ? da + 1 : db);
+        }
+    }
+
+    free(m);
+
+    return d;
+}
---
+++ levenshtein.h
@@ -0,0 +1,7 @@
+#ifndef __LEVENSHTEIN_H_LOADED__
+#define __LEVENSHTEIN_H_LOADED__
+
+int
+levenshtein(char *a, int a_size, char *b, int b_size);
+
+#endif // __LEVENSHTEIN_H_LOADED__
