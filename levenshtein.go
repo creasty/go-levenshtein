88fcd0f6e9ebb53b9b934f4d1358407c64d5c5e1
Yuki Iwanaga <yuki@creasty.com>
2016-09-28 23:55:40 +0900

    Fix

--- levenshtein.go
+++ levenshtein.go
@@ -19,6 +19,6 @@ func Distance(a, b string) int {
 	cb := C.CString(b)
 	defer C.free(unsafe.Pointer(cb))
 
-	cdist := C.levenshtein(a, C.int(len(a)), b, C.int(len(b)))
+	cdist := C.levenshtein(ca, C.int(len(a)), cb, C.int(len(b)))
 	return int(cdist)
 }
